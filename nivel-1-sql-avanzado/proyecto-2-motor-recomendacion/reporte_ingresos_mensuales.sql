62881addf51fe17faa60193bd705fa3f0703c8ac
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2026-06-13 21:06:59 -0500

    Reporte ingresos mensuales por categoria en los últimos 6 meses respecto al último pago

---
+++ nivel-1-sql-avanzado/proyecto-2-motor-recomendacion/reporte_ingresos_mensuales.sql
@@ -0,0 +1,40 @@
+WITH category_monthly_revenue AS (
+	-- Paso 1: Ingresos totales de cada categoria por mes
+	SELECT fc.category_id,
+		c.name AS category,
+		DATE_TRUNC('month', p.payment_date) AS month,
+		SUM(p.amount) AS total_revenue
+	FROM rental r
+	INNER JOIN payment p
+		ON r.rental_id = p.rental_id
+	INNER JOIN inventory i
+		ON r.inventory_id = i.inventory_id
+	INNER JOIN film_category fc
+		ON i.film_id = fc.film_id
+	INNER JOIN category c
+		ON fc.category_id = c.category_id
+	GROUP BY fc.category_id, c.name, DATE_TRUNC('month', p.payment_date)
+), 
+last_month AS (
+	-- Paso 2: Ubicar el mes del último pago realizado
+    SELECT DATE_TRUNC('month', MAX(payment_date)) AS last_month
+    FROM payment
+)
+
+-- Reporte correspondiente a los últimos 6 meses
+SELECT category,
+	MAX(CASE WHEN month = last_month - INTERVAL '5 month' 
+        THEN total_revenue END) AS "Feb 2006",
+    MAX(CASE WHEN month = last_month - INTERVAL '4 month' 
+        THEN total_revenue END) AS "Mar 2006",
+    MAX(CASE WHEN month = last_month - INTERVAL '3 month' 
+        THEN total_revenue END) AS "Apr 2006",
+    MAX(CASE WHEN month = last_month - INTERVAL '2 month' 
+        THEN total_revenue END) AS "May 2006",
+    MAX(CASE WHEN month = last_month - INTERVAL '1 month' 
+        THEN total_revenue END) AS "Jun 2006",
+    MAX(CASE WHEN month = last_month 
+        THEN total_revenue END) AS "Jul 2006"
+FROM category_monthly_revenue
+CROSS JOIN last_month
+GROUP BY category;
